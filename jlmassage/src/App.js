6ed7e31d8904ab86e5f9649593b549bb97fbdc6a
git <akdflyinvee73@gmail.com>
2019-01-10 19:32:49 -0600

    Images and front page text.

--- jlmassage/src/App.js
+++ jlmassage/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './images/backpain.png';
 import './App.css';
 
 class App extends Component {
@@ -9,7 +9,18 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            What is a triggerpoint? 
+            Commonly referred to as a "knot",
+            triggerpoints are small contractions
+            within the muscle fibers that cause
+            tenderness and chronic discomfort.
+            They usually appear in the neck,
+            shoulders, and back due to occupational/physical
+            stress on the body or mental/emotional
+            stress. Releasing these points of muscular
+            tension can lead to the elimination of padding
+            and helps to maintain a healthy and
+            comfortable lifestyle.
           </p>
           <a
             className="App-link"
