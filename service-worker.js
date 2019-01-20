d618fd6750070483f815b6d5fe7d460fc31d4a4d
git <akdflyinvee73@gmail.com>
2019-01-20 06:00:15 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/jlmassage/precache-manifest.c81d26ad1a18279033b56a57d52caa9a.js"
+  "/jlmassages/precache-manifest.005b8c3b38694c304fe5eddb1421d23d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/jlmassage/index.html", {
+workbox.routing.registerNavigationRoute("/jlmassages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
