cb3d576e55e9c776e6b62ce3346eb73b13dac8fd
Anthony Dombrowski <akdflyinvee73@gmail.com>
2019-01-23 21:21:37 -0600

    Updates

---
+++ precache-manifest.946b24a6a5898faebfac87e1b0f6a926.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "933ae3b1deff22e245d61706f6404126",
+    "url": "/jlmassage/static/media/bg.933ae3b1.jpg"
+  },
+  {
+    "revision": "70f9ff989d17e964812ad41b83bd91cd",
+    "url": "/jlmassage/static/media/backpain.70f9ff98.png"
+  },
+  {
+    "revision": "de9246a6c2fcecfd7838",
+    "url": "/jlmassage/static/js/runtime~main.de9246a6.js"
+  },
+  {
+    "revision": "89dd08d084e16325541e",
+    "url": "/jlmassage/static/js/main.89dd08d0.chunk.js"
+  },
+  {
+    "revision": "587317a2c0b60a13a19f",
+    "url": "/jlmassage/static/js/1.587317a2.chunk.js"
+  },
+  {
+    "revision": "89dd08d084e16325541e",
+    "url": "/jlmassage/static/css/main.8df6b4b4.chunk.css"
+  },
+  {
+    "revision": "587317a2c0b60a13a19f",
+    "url": "/jlmassage/static/css/1.6dff5397.chunk.css"
+  },
+  {
+    "revision": "c7af100b94d22aebaf00c95cbbe213c3",
+    "url": "/jlmassage/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3e6c625820b3b88ddbdfe58bb607b094.js"
+  "/jlmassage/precache-manifest.946b24a6a5898faebfac87e1b0f6a926.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/jlmassage/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
